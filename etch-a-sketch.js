64bcaf13d55e5e65cbb804776ee1090719d6362d
Kondorcom <kondorcom88@gmail.com>
2022-07-07 05:03:54 +0200

    Add random color change

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -26,10 +26,25 @@ for (let i = 0; i < allElements.length; i++) {
     allElements[i].addEventListener('mouseover', function() {
     console.clear();
     console.log("You clicked:", this);
-    this.style.backgroundColor = 'black';     
+    let randomClr = randomColor();
+
+    console.log("color:", randomClr);
+    this.style.backgroundColor = randomClr;  
+    // this.style.backgroundColor = 'black';  
+    // this.style.backgroundColor = 'rgb('+66+','+107+','+87+')';
+       
          });
      }
     }
+function randomColor(){
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+const r = randomBetween(0, 255);
+const g = randomBetween(0, 255);
+const b = randomBetween(0, 255);
+const rgb2 = "rgb("+[r,g,b].join() + ")";
+const rgb = `rgb('+${r}+','+${g}+','+${b}+')`; 
+    return rgb2;
+}
 
 function gridSize(){
    let grid = prompt('Please enter grid size', 20);
